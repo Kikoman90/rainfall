99154a6c0805981c208e2153ce61714b5a8d2e08
Francois SIDLER <fsidler@e2r6p4.42.fr>
2019-12-09 21:31:43 +0100

    level8

--- level8/source.c
+++ level8/source.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-unsigned int	*auth;
-char			*service;
+char	*auth;
+char	*service;
 
-int			main(void)
+int	main(void)
 {
 	char	str[128];
-	int		i;
 
-	while (42)
+	while (1)
 	{
-		printf("%p, %p\n", auth, service);
-		if (fgets(str, 128, stdin) == 0)
-			return (0);
+		printf("%p, %p \n", auth, service);
+
+		if (!fgets(str, 128, stdin))
+			break;
+
 		if (strncmp(str, "auth ", 5) == 0)
 		{
 			auth = malloc(4);
-			*auth = 0;
+			auth[0] = 0;
 			if (strlen(str + 5) < 30)
-				strcpy((char *)auth, &str[5]);
+				strcpy((char *)auth, str + 5);
 		}
 		if (strncmp(str, "reset", 5) == 0)
 			free(auth);
@@ -31,7 +32,7 @@ int			main(void)
 			if (auth[32] != 0)
 				system("/bin/sh");
 			else
-				fwrite("Password:\n", 10, 1, stdout);
+				fwrite("Password:\n", 1, 10, stdout);
 		}
 	}
 	return (0);
