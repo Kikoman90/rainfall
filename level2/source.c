7516e8140b1b92cb2391afafc3b2ccbafa898257
Francois Sidler <francoissidler@hotmail.fr>
2022-01-07 23:05:17 +0100

    don't pay attention 2

--- level2/source.c
+++ level2/source.c
@@ -4,15 +4,15 @@
 
 char	*p(void)
 {
-	char		buff[64];
-	unsigned long	ret_addr;
+	char	buff[64];
+	void	*ret_addr;
 
 	fflush(stdout);
 	gets(buff);
-	ret_addr = (unsigned long)__builtin_return_address(0);
-	if ((ret_addr & 0xb0000000) == 0xb0000000)
+	ret_addr = __builtin_return_address(0);
+	if (((unsigned int)ret_addr & 0xb0000000) == 0xb0000000)
 	{
-		printf("(%p)\n", (void*)ret_addr);
+		printf("(%p)\n", ret_addr);
 		exit(1);
 	}
 	puts(buff);
